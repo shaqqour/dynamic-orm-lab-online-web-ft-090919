0f3aa9cb6ad51d4319c4591e8f04adc9e6f5aae1
Shaqqour M <mshaqqourm@gmail.com>
2019-10-29 01:29:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    self.column_names.each do |property|
+        attr_accessor property.to_sym
+    end
 end
